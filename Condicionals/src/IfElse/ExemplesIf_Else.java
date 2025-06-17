2a8c469bacb57d7e5006e8d03c421aea08ebff4f
Pau <paugarrigacomas@gmail.com>
2025-06-17 11:33:16 +0200

    condicionals

---
+++ Condicionals/src/IfElse/ExemplesIf_Else.java
@@ -0,0 +1,17 @@
+package IfElse;
+
+public class ExemplesIf_Else {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nota = 10;
+		
+		if (nota>5) {
+			System.out.print("Has aprovat");
+		}else {
+			System.out.print("Has de repetir");
+		}
+
+	}
+
+}
